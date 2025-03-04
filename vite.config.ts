c6d9400549b8681908637ee0fe820cfc158cfb7d
shaggun <shaggynetworks@gmail.com>
2025-03-04 17:44:39 -0600

    Update config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL || '/',
+  base: import.meta.env.VITE_BASE_URL || '/',
   build: {
     outDir: 'dist',
   }
