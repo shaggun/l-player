0cc6173bb267851c0eca5988bb6641bed37b3738
shaggun <shaggynetworks@gmail.com>
2025-03-04 18:28:44 -0600

    Update configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: import.meta.env.VITE_BASE_URL || '/',
-  build: {
-    outDir: 'dist',
-  }
-})
+export default defineConfig(({ mode }) => {
+  // Load environment variables from the .env file
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_URL || '/',
+    build: {
+      outDir: 'dist',
+    },
+  };
+});
